c42da2552c8dbe6f414c66163e525d8ba187d368
usr42 <usr42@users.noreply.github.com>
2018-04-22 21:59:25 +0200

    Add cs tool to use clair-scanner directly

--- bin/scan_all_local_images.sh
+++ bin/scan_all_local_images.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-clair_ip=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2| cut -d' ' -f1)
 mkdir /tmp/report/
 docker ps --format='{{.Image}}' | sort -u | while read image
-do 
-    clair-scanner -c=http://clair:6060 --ip=$clair_ip -r /tmp/report/$(echo $image|md5sum |cut -d' ' -f1).json $image > /dev/null 2> /dev/null
+do
+    cs -r /tmp/report/$(echo $image|md5sum |cut -d' ' -f1).json $image > /dev/null 2> /dev/null
 done
 
 if [ -n "$(ls -A /tmp/report)" ]
