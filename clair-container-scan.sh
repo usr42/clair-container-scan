029118f5a34ad6c8c267386f6c3de4c746499f0f
usr42 <usr42@users.noreply.github.com>
2018-05-07 11:46:25 +0200

    Add -p option to pull images befor running
    By default images are not pulled anymore

--- clair-container-scan.sh
+++ clair-container-scan.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
+usage() {
+    echo "Usage: $0 [-p]"
+    echo
+    echo "Options:"
+    echo " -p : Pull images before running scan"
+    exit 1
+}
+
+PULL=0
+
+while getopts ":ph" opt; do
+    case $opt in
+        p)
+            PULL=1
+            shift
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            ;;
+        h)
+            usage
+            ;;
+    esac
+done
+
 BASEDIR=$(cd $(dirname "$0") && pwd)
 
 cd "$BASEDIR"
-docker-compose pull
+[ "$PULL" == 1 ] && docker-compose pull
 docker-compose run --rm scanner "$@" #2>/dev/null
 docker-compose down >/dev/null #2>&1
